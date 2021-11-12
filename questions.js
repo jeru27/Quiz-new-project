82efe49d3f702ab200c6751b03491a2f673723ba
jeru27 <jrumora@gmail.com>
2021-11-12 09:41:52 -0800

    first commit

---
+++ questions.js
@@ -0,0 +1,184 @@
+// creating and array for questions, number, options, and answers
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "HyperText Markup Language",
+        options: [
+            "HyperText Markup Language",
+            "HoverTitle Markdow Language",
+            "HyperTitle Markup Letter",
+            "HighText Matter Language"
+        ]
+
+    },
+    {
+        numb: 2,
+        question: "What is the difference between Java and JavaScript?",
+        answer: "Java is an OOP programming language while JavaScript is an OOP scripting language",
+        options: [
+            "JavaScript is an OOP programming language while Java is an OOP scripting languag",
+            "Java is an OOP programming language while JavaScript is an OOP scripting language",
+            "Both Java and JavaScript are OOP programing and scripting languages",
+            "Neither Java or JavaScript are OOP programing and scripting languages"
+        ]
+
+    },
+    {
+        numb: 3,
+        question: "What does CSS stand for?",
+        answer: "Cascade Style Sheets",
+        options: [
+            "Correlational Style Standard",
+            "Cursive Style Sheets",
+            "Cascade Style Standard",
+            "Cascade Style Sheets"
+        ]
+
+    },
+    {
+        numb: 4,
+        question: "What year was JavaScript created?",
+        answer: "1995",
+        options: [
+            "1987",
+            "1995",
+            "1988",
+            "1992"
+        ]
+
+    },
+    {
+        numb: 5,
+        question: "What are the three variables in JavaScript?",
+        answer: "var, let, const",
+        options: [
+            "main, now, script",
+            "let, main, script",
+            "var, let, const",
+            "var, let, script"
+        ]
+
+    },
+    {
+        numb: 6,
+        question: "What does ECMA Script stand for?",
+        answer: "European Computer Manufacturers Association Script",
+        options: [
+            "European Computer Manufacturers Association Script",
+            "European Computing Malfunction Association Standard",
+            "Euroasian Computing Made Alternate Style",
+            "Eastern Cascade Monodimensional Algoritam Standard"
+        ]
+
+    },
+    {
+        numb: 7,
+        question: "What method adds and element at the end of an array?",
+        answer: "Push",
+        options: [
+            "Unshift",
+            "Shift",
+            "Pop",
+            "Push"
+        ]
+
+    },
+    {
+        numb: 8,
+        question: "How do we add properties to objects?",
+        answer: "With dot and bracket notation",
+        options: [
+            "With dot notation",
+            "With dot and bracket notation",
+            "With bracket notation",
+            "With curly braces"
+        ]
+
+    },
+    {
+        numb: 9,
+        question: "What are nested data structures?",
+        answer: "All of the above",
+        options: [
+            "Objects in arrays",
+            "Objects in objects",
+            "Arrays in arrays",
+            "All of the above"
+        ]
+
+    },
+    {
+        numb: 10,
+        question: "What does DOM stand for?",
+        answer: "Document Object Model",
+        options: [
+            "Dominant Object Model",
+            "Delivery Oriented Model",
+            "Document Object Model",
+            "Double Order Master"
+        ]
+
+    },
+    {
+        numb: 11,
+        question: "How many levels of headings are there in HTML?",
+        answer: "6",
+        options: [
+            "2",
+            "5",
+            "3",
+            "6"
+        ]
+
+    },
+    {
+        numb: 12,
+        question: "What is the sign of ID selectors in CSS?",
+        answer: "#",
+        options: [
+            "*",
+            "&",
+            "#",
+            "@"
+        ]
+
+    },
+    {
+        numb: 13,
+        question: "What does RGB color stand stand for?",
+        answer: "Red, green, blue.",
+        options: [
+            "Red, gray , blue",
+            "Rose, green, brown",
+            "Red, green, blue",
+            "Rose, gray, brown"
+        ]
+
+    },
+    {
+        numb: 14,
+        question: "What does < p > stand for?",
+        answer: "Paragraph",
+        options: [
+            "Postmate",
+            "Public",
+            "Paragraph",
+            "Place"
+        ]
+
+    },
+    {
+        numb: 15,
+        question: "What is < span > ?",
+        answer: "Inline element of body.",
+        options: [
+            "Inline element of body.",
+            "Block element of body.",
+            "Inline element of head.",
+            "Block element of head."
+        ]
+
+    }
+];
